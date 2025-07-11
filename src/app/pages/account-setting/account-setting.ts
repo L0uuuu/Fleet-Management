22f73757f0afb7da021d413e00eaa58628976130
L0uuuu <louaiboubaker@gmail.com>
2025-07-11 15:20:47 +0100

    modified the htm and css files to improve the layout and responsiveness of the account settings page. The changes include adjusting the height of the bottom section, ensuring that it can scroll if content overflows, and making sure the overall layout is flexible and adapts to different screen sizes.

--- src/app/pages/account-setting/account-setting.ts
+++ src/app/pages/account-setting/account-setting.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+//icon importation
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-account-setting',
   standalone: false,
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './account-setting.scss'
 })
 export class AccountSetting {
+  private firstName: string = 'John';
+  private lastName: string = 'Doe';
+  private email: string = 'louaiboubaker@gmail.com';
+  private phone: string = '+216 22 222 222';
+  //getters
+  get getFirstName(): string {
+    return this.firstName;
+  }
+  get getLastName(): string {
+    return this.lastName;
+  }
+  get getEmail(): string {
+    return this.email;
+  }
+  get getPhone(): string {
+    return this.phone;
+  }
+  //icons
+  faUserCircle = faUserCircle;
 
 }
