94cff20c0e8ed6d85e1c9116e416efe24d78b831
L0uuuu <louaiboubaker@gmail.com>
2025-07-15 00:04:33 +0100

    minor adjustments to routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
       { path: 'agency', component:  Agency },
       { path: 'date', component:  AppointmentDate },
       { path: 'summary', component: Summary },
-      { path: '', redirectTo: 'summary', pathMatch: 'full' }
+      { path: '', redirectTo: 'agency', pathMatch: 'full' }
     ]
   }
 ];
