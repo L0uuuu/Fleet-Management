876a05dee2a74be07df0950ae9a23918f355a6eb
L0uuuu <louaiboubaker@gmail.com>
2025-07-21 19:56:45 +0100

    minor tweaks

--- src/app/pages/vehicles/vehicles.ts
+++ src/app/pages/vehicles/vehicles.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+
+//icon importation
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-vehicles',
   standalone: false,
@@ -7,5 +12,7 @@ import { Component } from '@angular/core';
   styleUrl: './vehicles.scss'
 })
 export class Vehicles {
-
+  //icon
+  faPlus = faPlus;
+  faSearch = faSearch;
 }
