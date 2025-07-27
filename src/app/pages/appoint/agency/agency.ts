08f1fe36145a9ad6b392e241e6d26e610794a656
L0uuuu <louaiboubaker@gmail.com>
2025-07-27 19:37:48 +0100

    minor adjusments

--- src/app/pages/appoint/agency/agency.ts
+++ src/app/pages/appoint/agency/agency.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { AgencyAPI } from '../../../services/appoint/agency-api';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-agency',
   standalone: false,
   templateUrl: './agency.html',
   styleUrl: './agency.scss'
 })
-export class Agency {
+export class Agency implements OnInit {
+  constructor(private router: Router,private agencyAPI:AgencyAPI){}
+
+  ngOnInit(): void {
+    const selectedServiceId =sessionStorage.getItem('selectedServiceId');
+  }
+
+
 
   //icon
   faChevronRight = faChevronRight;
