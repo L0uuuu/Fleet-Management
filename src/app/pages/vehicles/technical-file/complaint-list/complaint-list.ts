f48db77c89efa45679550c6cc81040f665a698b9
L0uuuu <louaiboubaker@gmail.com>
2025-08-20 00:47:44 +0100

    display of the complaint list

--- src/app/pages/vehicles/technical-file/complaint-list/complaint-list.ts
+++ src/app/pages/vehicles/technical-file/complaint-list/complaint-list.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { VehiclesAPI } from '../../../../services/vehicles-api';
 import { VehiclesLog } from '../../../../services/vehiclesLogs/vehicles-log';
 
+import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-complaint-list',
   standalone: false,
@@ -9,7 +11,7 @@ import { VehiclesLog } from '../../../../services/vehiclesLogs/vehicles-log';
   styleUrl: './complaint-list.scss'
 })
 export class ComplaintList implements OnInit {
-  constructor(private vehiclesAPI:VehiclesAPI,private vehiclesLog:VehiclesLog) {}
+  constructor(private vehiclesAPI:VehiclesAPI,private vehiclesLog:VehiclesLog,private cdr: ChangeDetectorRef) {}
 
   vehiclesID: string | null = null;
   brand: string = '';
@@ -28,12 +30,26 @@ export class ComplaintList implements OnInit {
       )
         .subscribe(response => {
           this.complaints = response;
+          this.cdr.detectChanges();
           console.log('complaints:', this.complaints);
         });
     } else {
       console.warn('vehiclesID is null. Skipping API call.');
     }
   }
+  getStatusColor(status: string): string {
+    switch (status) {
+      case 'REQUESTED':
+        return 'grey';
 
+      case 'ACCEPTED':
+        return 'green';
+
+      case 'REJECTED':
+        return 'red';
+      default:
+        return 'black'; // Default color for unknown status
+    }
+  }
 
 }
